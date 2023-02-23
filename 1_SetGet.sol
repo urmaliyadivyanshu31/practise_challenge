1652e966bcfdee6d59cd5180467b6e2b218b9b2a
Divyanshu Urmaliya <95743189+urmaliyadivyanshu31@users.noreply.github.com>
2023-02-23 19:56:51 +0530

    Updated Code

--- 1_SetGet.sol
+++ 1_SetGet.sol
@@ -5,17 +5,20 @@ pragma solidity >=0.7.0 <0.9.0;
 contract GetSet {
 
     
-    uint num;
+    uint num1;
+    uint num2;
     string letter;
+    address addr;
 
-    function get() public view returns( uint, string memory) {
-        return ( num, letter);
+    function get() public view returns( uint,uint,address, string memory) {
+        return ( num1, num2, addr, letter);
       
     }
 
-    function set(uint _num ,string memory _letter ) public  {
-      
-        num = _num;
+    function set(uint _num1, uint _num2 ,string memory _letter,address _addr ) public  { 
+        num1 = _num1;
+        num2 = _num2;
         letter = _letter;
+        addr = _addr;
     }
 }
